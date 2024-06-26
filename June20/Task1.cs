c53501628d3bcdae01764e0f8fc2e7a0bd90cb46
Disha Kalonje <dishavinodkalonje6148@gmail.com>
2024-06-26 09:53:09 +0530

    Task1.cs

---
+++ June20/Task1.cs
@@ -0,0 +1,36 @@
+ // Connect and Query a Database Write a C# program that connects to a SQL database and retrieves the names of all tables within it. Use SqlConnection and SqlCommand objects. 
+
+using System.Threading.Tasks;
+namespace DbSQL
+{
+  internal class Program
+  {
+      static void Main(string[] args)
+      {
+      
+        string con = "data source-DESKTOP-TIC5DM4\\SQLEXPRESS; initial catalog employee; integrated
+      
+        SqlConnection connection = new SqlConnection(con);
+      
+        string command = "select name from sys.tables";
+      
+        SqlCommand cmd = new SqlCommand(command, connection);
+      
+        connection.Open();
+      
+        SqlDataReader reader = cmd.ExecuteReader();
+      
+        Console.WriteLine("Tables are: ");
+      
+        while (reader.Read()) 
+      
+        {
+      
+          Console.WriteLine(reader.GetString(0));
+      
+        }
+   
+        connection.Close();
+    }
+  }
+}
